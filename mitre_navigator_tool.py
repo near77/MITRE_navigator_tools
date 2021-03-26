5e518a93c06fccc66caee3977aefc306d731badf
Near77 <near6334@gmail.com>
2021-03-26 12:19:58 +0800

    version 1.0 completed.

--- mitre_navigator_tool.py
+++ mitre_navigator_tool.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-
+import argparse
 
 def load_navigator_data(json_file):
     with open(json_file) as f:
@@ -8,27 +8,40 @@ def load_navigator_data(json_file):
         coverage_df = pd.DataFrame(data["techniques"])
     return coverage_df
 
-coverage_json_file = "./Coverage.json"
-coverage_df = load_navigator_data(coverage_json_file)# 540
-APT29_json_file = "./APT29.json"
-APT29_df = load_navigator_data(APT29_json_file)
-# print(coverage_df)
-join_cov_apt29 = pd.merge(coverage_df,APT29_df[["techniqueID", "tactic"]],on=["techniqueID", "tactic"])# 34
-# print(join_cov_apt29)
-tmp_df = APT29_df.append(coverage_df)
-tmp_df = tmp_df.append(coverage_df)
-diff_cov_apt29 = tmp_df.drop_duplicates(subset=["techniqueID", "tactic"],keep=False)# 4
-# print(diff_cov_apt29)
-tmp_df = coverage_df.append(join_cov_apt29) # 540 + 34 = 574
-coverage_df = tmp_df.drop_duplicates(subset=["techniqueID", "tactic"], keep=False)
-join_cov_apt29["color"] = "#ffe766"
-coverage_df = coverage_df.append(join_cov_apt29)
-coverage_df = coverage_df.append(diff_cov_apt29)
 
-cov_dic = coverage_df.to_dict('records')
+def mitre_navigator_tool(primary:str, secondary:str, store_path:str) -> None:
+    """
+    :param primary: primary MITRE navigator table path (.json)
+    :param secondary: secondary MITRE navigator table path (.json)
+    :param store_path: the path to store the result (.json)
+    """
+    primary_df = load_navigator_data(primary)
+    secondary_df = load_navigator_data(secondary)
+    intersection_df = pd.merge(primary_df, secondary_df[["techniqueID", "tactic"]], on=["techniqueID", "tactic"])
+    intersection_df["color"] = "#ffe766"
+    substraction_df = secondary_df.append(primary_df)
+    substraction_df = substraction_df.append(primary_df)
+    substraction_df = substraction_df.drop_duplicates(subset=["techniqueID", "tactic"],keep=False)
+    result_df = primary_df.append(intersection_df)
+    result_df = result_df.drop_duplicates(subset=["techniqueID", "tactic"], keep=False)
+    result_df = result_df.append(intersection_df)
+    result_df = result_df.append(substraction_df)
+    result_dic = result_df.to_dict("records")
+    with open(primary) as f:
+        data = json.load(f)
+        with open(store_path, "w") as json_f:
+            data["techniques"] = result_dic
+            json.dump(data, json_f, indent=4) 
 
-with open(coverage_json_file) as f:
-    data = json.load(f)
-    with open("./cov_apt29.json", "w") as json_f:
-        data["techniques"] = cov_dic
-        json.dump(data, json_f, indent=4)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--primary",help = "Primary MITRE navigator table path (.json)")
+    parser.add_argument("--secondary", help = "Secondary MITRE navigator table path (.json)")
+    parser.add_argument("--store_path", help = "The path to store the result (.json)")
+    args = parser.parse_args()
+    primary = args.primary
+    secondary = args.secondary
+    store_path = args.store_path
+    mitre_navigator_tool(primary, secondary, store_path)
+
