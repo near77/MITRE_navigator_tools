d66161e7babae94e15b8f6d69405806097e7c2c2
Near77 <near6334@gmail.com>
2021-04-02 12:44:06 +0800

    Add select technique when subtechnique is selected.

--- mitre_navigator_tool.py
+++ mitre_navigator_tool.py
@@ -2,23 +2,52 @@ import json
 import pandas as pd
 import argparse
 
-def load_navigator_data(json_file):
+Green = "#c7e9c0"
+Red = "#fca2a2"
+Yellow = "#fcf3a2"
+
+def load_navigator_data(json_file, color):
     with open(json_file) as f:
         data = json.load(f)
         coverage_df = pd.DataFrame(data["techniques"])
+        coverage_df["color"] = color
     return coverage_df
 
+def color_techniques_with_subtecnique(primary:str, color:str) -> pd.DataFrame:
+    """
+    :param primary: primary MITRE navigator table path (.json)
+    :param color: color to fill in the cell ("#ffffff")
+    """
+    all_technique_df = load_navigator_data("./Navigator_tables/All_techniques.json", color)
+    primary_df = load_navigator_data(primary, Green)
+    primary_technique_set = [x for index, x in primary_df.iterrows()]
+    primary_technique_set = list(set([x["techniqueID"] for index, x in primary_df.iterrows()]))
+    lost_techniques = []
+    for technique in primary_technique_set:
+        if technique.split(".")[0] not in primary_technique_set:
+            lost_techniques.append(technique.split(".")[0])
+    lost_techniques_df = all_technique_df[all_technique_df["techniqueID"].isin(lost_techniques)]
+    result_df = primary_df.append(lost_techniques_df)
+    result_df["color"] = color
+    return result_df
 
-def mitre_navigator_tool(primary:str, secondary:str, store_path:str) -> None:
+
+def mitre_navigator_tool(primary:str, secondary:str, store_path:str, show_meta:bool = True) -> None:
     """
     :param primary: primary MITRE navigator table path (.json)
     :param secondary: secondary MITRE navigator table path (.json)
     :param store_path: the path to store the result (.json)
+    :param show_meta: if include technique of selected subtechniques
     """
-    primary_df = load_navigator_data(primary)
-    secondary_df = load_navigator_data(secondary)
+    if show_meta:
+        primary_df = color_techniques_with_subtecnique(primary, Green)
+        secondary_df = color_techniques_with_subtecnique(secondary, Red)
+    else:
+        primary_df = load_navigator_data(primary, Green)
+        secondary_df = load_navigator_data(secondary, Red)
+    
     intersection_df = pd.merge(primary_df, secondary_df[["techniqueID", "tactic"]], on=["techniqueID", "tactic"])
-    intersection_df["color"] = "#ffe766"
+    intersection_df["color"] = Yellow
     substraction_df = secondary_df.append(primary_df)
     substraction_df = substraction_df.append(primary_df)
     substraction_df = substraction_df.drop_duplicates(subset=["techniqueID", "tactic"],keep=False)
@@ -33,7 +62,7 @@ def mitre_navigator_tool(primary:str, secondary:str, store_path:str) -> None:
             data["techniques"] = result_dic
             json.dump(data, json_f, indent=4) 
 
-
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--primary", help = "Primary MITRE navigator table path (.json)")
